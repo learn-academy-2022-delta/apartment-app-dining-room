b0b7cbe355a18f4617500750dac148c7655639f2
Jordyn Jensen <jordyneajensen@gmail.com>
2022-08-09 14:27:26 -0700

    added text and buttons with links to the home page

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
+import { Nav, NavItem } from 'reactstrap'
 
 export default class Home extends Component {
   render() {
+    const {
+      new_user_route,
+      sign_in_route,
+    } = this.props
     return (
-      <h3>Home</h3>
+      <div>
+        <h3>Welcome!</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non accumsan cursus tristique parturient faucibus cras non amet lectus. Placerat netus viverra tincidunt sed enim varius volutpat sed. Commodo arcu mauris nisl malesuada tincidunt enim. Amet, mauris scelerisque morbi penatibus sodales scelerisque sed. Adipiscing purus aliquam dui mi, sit dui lorem sit mattis. Consequat nunc congue arcu tempor ut iaculis risus nunc consectetur.</p>
+        <Nav>
+          <NavItem>
+                <a href={sign_in_route} className="nav-link">Sign In</a>
+          </NavItem>
+          <NavItem>
+                <a href={new_user_route} className="nav-link">Sign Up</a>
+          </NavItem>
+        </Nav>
+      </div>
     )
   }
 }
