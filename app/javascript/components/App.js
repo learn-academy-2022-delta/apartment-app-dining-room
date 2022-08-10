eb64fb37b100c5d431a843964e0c9e09e56e06aa
Jordyn Jensen <jordyneajensen@gmail.com>
2022-08-10 14:08:18 -0700

    show branch rebase

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -39,7 +39,10 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/apartmentindex" render={(props) => <ApartmentIndex apartments={this.state.apartments}/>} />
-            <Route path="/apartmentshow" component={ApartmentShow} />
+            <Route path="/apartmentshow/:id" render={(props) => {
+              let id = props.match.params.id
+              let apartment = this.state.apartments.find(apartment => apartment.id === +id)
+              return <ApartmentShow apartment={apartment}/> }} />
             <Route path="/apartmentnew" component={ApartmentNew} />
             <Route path="/apartmentedit" component={ApartmentEdit} />
             <Route component={NotFound}/>
