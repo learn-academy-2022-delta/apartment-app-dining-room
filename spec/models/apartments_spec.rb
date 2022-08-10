bdf27026625f64b7106e7c259cc3bed778476711
Ricky Fort <100113249+AriseSon@users.noreply.github.com>
2022-08-10 14:40:50 -0700

    NOW I'M FINISHED

--- spec/models/apartments_spec.rb
+++ spec/models/apartments_spec.rb
@@ -23,67 +23,68 @@ RSpec.describe Apartment, type: :model do
   end
 
   it 'user cannot create an apartment without street' do
-    apartment = user.apartments.create street: '', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs.', bedrooms: 7, bathrooms: 7, image: "http//facebook12345", user_id: 'SzaSeason'
+    apartment = user.apartments.create city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs.', bedrooms: 7, bathrooms: 7, image: "http//facebook12345", user_id: 11212
 
     expect(apartment.errors[:street]).to_not be_empty
   end
 
     it 'is not valid without a city' do
-      apartment = user.apartments.create street: 'Canal ST', city: '', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs.', bedrooms: 7, bathrooms: 7, image: "http//facebook12345", user_id: 'SzaSeason'
+      apartment = user.apartments.create street: 'Canal ST', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs.', bedrooms: 7, bathrooms: 7, image: "http//facebook12345", user_id: 11212
       
       expect(apartment.errors[:city]).to_not be_empty
   end
 
   it 'is not valid without a state' do
-    apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs.', bedrooms: 7, bathrooms: 7, image: "http//facebook12345", user_id: 'SzaSeason'
+    apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs.', bedrooms: 7, bathrooms: 7, image: "http//facebook12345", user_id: 11212
 
     expect(apartment.errors[:state]).to_not be_empty
   end
 
   it 'is not valid without a manager' do
-    apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: '', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs.', bedrooms: 7, bathrooms: 7, image: "http//facebook12345", user_id: 'SzaSeason'
+    apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs.', bedrooms: 7, bathrooms: 7, image: "http//facebook12345", user_id: 11212
 
     expect(apartment.errors[:manager]).to_not be_empty
   end
 
 it 'is not valid without a email' do
-  apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: '', price: 'unlimited', pets: '2 small dogs.', bedrooms: 7, bathrooms: 7, image: "http//facebook12345", user_id: 'SzaSeason'
+  apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', price: 'unlimited', pets: '2 small dogs.', bedrooms: 7, bathrooms: 7, image: "http//facebook12345", user_id: 11212
 
   expect(apartment.errors[:email]).to_not be_empty
   end
 
 it 'is not valid without a price' do
-  apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: '', pets: '2 small dogs.', bedrooms: '7', bathrooms: '7', image: "http//facebook12345", user_id: 'SzaSeason'
+  apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', pets: '2 small dogs.', bedrooms: '7', bathrooms: '7', image: "http//facebook12345", user_id: 11212
 
   expect(apartment.errors[:price]).to_not be_empty
 end
 
 it 'is not valid without a pets' do
-  apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '', bedrooms: 7, bathrooms: 7, image: "http//facebook12345", user_id: 'SzaSeason'
+  apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', bedrooms: 7, bathrooms: 7, image: "http//facebook12345", user_id: 11212
 
   expect(apartment.errors[:pets]).to_not be_empty
 end
 
 it 'is not valid without a bedrooms' do
-  apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs', bedrooms: '', bathrooms: 7, image: "http//facebook12345", user_id: 'SzaSeason'
+  apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs', bathrooms: 7, image: "http//facebook12345", user_id: 11212
 
   expect(apartment.errors[:bedrooms]).to_not be_empty
 end
 
 it 'is not valid without a bathrooms' do
-  apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs', bedrooms: 7, bathrooms: '', image: "http//facebook12345", user_id: 'SzaSeason'
+  apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs', bedrooms: 7, image: "http//facebook12345", user_id: 11212
 
   expect(apartment.errors[:bathrooms]).to_not be_empty
 end
 
 it 'is not valid without a image' do
-  apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs', bedrooms: 7, bathrooms: 7, image: "" , user_id: 'SzaSeason'
+  apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs', bedrooms: 7, bathrooms: 7, user_id: 11212
 
   expect(apartment.errors[:image]).to_not be_empty
 end
 
 it 'is not valid without a user_id' do
-  apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs', bedrooms: 7, bathrooms: 7, image: "http//facebook12345", user_id: ""
+  apartment = user.apartments.create street: 'Canal ST', city: 'New Orleans', state: "The Boot", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs', bedrooms: 7, bathrooms: 7, image: "http//facebook12345"
+
  expect(apartment.errors[:user_id]).to be_empty
    end
 end
