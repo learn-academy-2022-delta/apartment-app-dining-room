56cd83ebcb64db3667ac1dc1509118b3be50a918
Ricky Fort <100113249+AriseSon@users.noreply.github.com>
2022-08-10 09:06:36 -0700

    Validations Completed

--- spec/models/apartments_spec.rb
+++ spec/models/apartments_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Apartment, type: :model do
-  it("Apartment model exists") do
-  expect{Apartment.create}.to_not raise_error 
+  it 'is not valid without a user_id' do
+    Sza = User.create street: 'Main St', city: 'West coast', state: "West Coast", manager: 'My own Boss', email: 'szaseason@yahoo.com', price: 'unlimited', pets: '2 small dogs.', bedrooms: '7', bathrooms: '7', image: "N/A",
+
+    expect(Sza.errors[:user_id]).to_not be_empty
+
   end
 end
