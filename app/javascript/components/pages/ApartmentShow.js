451ce0c0a60b9b1e546b4767dc75bf2f41884c1a
Jordyn Jensen <jordyneajensen@gmail.com>
2022-08-10 15:52:05 -0700

    mess of a time, all knotted up, show is working, test passes, deleted extra spaces in apartments controller because it was bugging me.

--- app/javascript/components/pages/ApartmentShow.js
+++ app/javascript/components/pages/ApartmentShow.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react'
 import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap'
 
 export default class ApartmentShow extends Component {
+
   render() {
     let { apartment } = this.props
+    console.log(apartment)
 
     return (
       <>
-        <div classname="show=container">
+        <div classname="show=container"> 
+        {apartment &&
           <Card id="showCard">
-            <CardImg src={apartment.image} />
+            <CardImg src={apartment.image}/>
             <CardBody>
               <CardTitle>{apartment.price}</CardTitle>
               <CardText>
@@ -21,6 +24,7 @@ export default class ApartmentShow extends Component {
               </CardText>
             </CardBody>
           </Card>
+        }
         </div>
       </>
     )
--- app/javascript/components/pages/ApartmentShow.test.js
+++ app/javascript/components/pages/ApartmentShow.test.js
@@ -14,9 +14,28 @@ import ApartmentShow from './ApartmentShow'
 Enzyme.configure({ adapter: new Adapter() })
 
 describe("When ApartmentShow renders", () => {
+  const apartment = {
+    user_id: 1,
+    street: "123 other way",
+    city: "nashville",
+    state: "Tennessee",
+    manager: "Steve",
+    email: "apartmetnOther@gmail.com",
+    price: "2100",
+    bedrooms: 2,
+    bathrooms: 1,
+    pets: "no",
+    image:"https://furnishrcdn-134f8.kxcdn.com/wp-content/uploads/2020/02/Sandy_apartment_daylight-e1584735599779.jpg",
+    }
+  
+  let renderedApartmentShow;
+
+  beforeEach(() => {
+    renderedApartmentShow = shallow(<ApartmentShow apartment={apartment} />);
+  });
+
   it("displays one Card", () => {
-    const apartmentShow = shallow(<ApartmentShow />)
-    const apartmentShowHeading = apartmentShow.find("Card")
+    const apartmentShowHeading = renderedApartmentShow.find("Card")
     expect(apartmentShowHeading.length).toEqual(1)
   })
 })
