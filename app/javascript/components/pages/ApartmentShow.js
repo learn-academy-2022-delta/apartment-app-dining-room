aa196b1589bf50cef8a097afee828a8d4401ccde
Jordyn Jensen <jordyneajensen@gmail.com>
2022-08-10 11:19:54 -0700

    testing versioin control

--- app/javascript/components/pages/ApartmentShow.js
+++ app/javascript/components/pages/ApartmentShow.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
+import { Card } from 'reactstrap'
 
 export default class ApartmentShow extends Component {
   render() {
     return (
-      <h3>ApartmentShow</h3>
+      <>
+        <div classname="show=container">
+          <Card id="showCard">
+            <Card.Img src={apartment.image} />
+            <Card.Body>
+              <Card.Title>{apartment.price}</Card.Title>
+              <Card.Text>
+                <span>{apartment.street}, {apartment.city}, {apartment.state}</span>
+                <br/>
+                <span>{apartment.bedrooms}, {apartment.bathrooms}</span>
+                <br/>
+                <span>{apartment.pets}</span>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </>
     )
   }
 }
